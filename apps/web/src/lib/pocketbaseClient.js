339fdaf60ee929fc180070c514cb65daf757275a
GRENOVERSE <grenoverse@gmail.com>
2026-04-20 23:51:59 +0530

    Update pocketbaseClient.js

--- apps/web/src/lib/pocketbaseClient.js
+++ apps/web/src/lib/pocketbaseClient.js
@@ -1,6 +1,6 @@
 import Pocketbase from 'pocketbase';
 
-const POCKETBASE_API_URL = "/hcgi/platform";
+const POCKETBASE_API_URL = "https://growperty.pockethost.io";
 
 const pocketbaseClient = new Pocketbase(POCKETBASE_API_URL);
 
